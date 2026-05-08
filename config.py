08487c73417c73bec16f8c51cfb2b2906ef14721
Arnav Mahajan <arnav.mahajan.2001@gmail.com>
2026-05-08 21:27:20 +1000

    Changes in Config

--- config.py
+++ config.py
@@ -9,3 +9,21 @@ MAX_NEW_TOKENS = 8192
 OLLAMA_BASE_URL = "http://localhost:11434"
 
 RESULTS_DIR = "results/raw"
+
+MODELS = {
+
+}
+
+BENCHMARKS = {
+    "gsm_symbolic",
+    "gsm_plus",
+    "gsm_ic",
+    "bigbench_hard",
+    "folio"
+}
+
+CONDITIONS = [
+    "zero_shot",
+    "random_few_shot",
+    "targeted_few_shot"
+]
