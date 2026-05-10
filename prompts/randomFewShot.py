3068366fbe83094e4f4bdd6a5be486197248fbad
Arnav Mahajan <arnav.mahajan.2001@gmail.com>
2026-05-10 19:51:35 +1000

    Added Prompts for new Datasets

--- prompts/randomFewShot.py
+++ prompts/randomFewShot.py
@@ -26,12 +26,18 @@ def randomFewShotPrompt(
             solution = ex.get("solution", f"Working through the question step by step the answer is {ans}")
             examples += f"Example {i}:\nProblem: {ques}\nSolution: {solution}\nAnswer: {ans}\n\n"
 
-        if benchmark in ["gsm_symbolic"]:
+        if benchmark in ["gsm_symbolic", "gsm_plus"]:
             instruction = (
                 "Given are some of the math problems with their solution\n"
                 "Follow the same step by step approach to solve the new problem.\n"
                 "At the end, write the final numeric answer starting with 'Answer:'"
             )
+        elif benchmark in ["folio"]:
+             instruction = (
+                "Given are some reasoning problems. \n"
+                "For each question, determine whether the conclusion is True, False, or Uncertain. \n"
+                "Follow the same format and at the end write 'Answer: True', 'Answer: False', or 'Answer: Uncertain'. "
+             )
         else:
             instruction = (
                 "Given are some problems with their solution\n"
--- prompts/zeroShot.py
+++ prompts/zeroShot.py
@@ -2,11 +2,18 @@
 # This is the baseline condition for all experiments.
 
 def zeroShotPrompt(question: str, benchmark: str) -> str:
-    if benchmark == "gsm_symbolic":
+    if benchmark in ["gsm_symbolic", "gsm_plus"]:
         instructions = (
             "You are a math teacher. Solve the following math problem step by step. Final answer will not be given\n"
             "Think about each step carefully\n"
             "At the end, Give the final answer in a new line starting with 'Answer: '"
+        )
+    elif benchmark in ["folio"]:
+        instructions = (
+            "Read the following statements carefully and determine whether the conclusion or answer per se is True or False, or Uncertain based on the information given to you. \n"
+            "Think the whole process step by step. \n"
+            "At the end, give the final answer in a new line starting with 'Answer: '\n"
+            "The answer must be exactly one of: True, False, Uncertain."
         )
     else: 
         instructions = (
